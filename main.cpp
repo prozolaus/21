3a434646d9d1e07790f1ef233e5c062b21cacdcd
Nikolai Prozorov <prozorovmykola@gmail.com>
2022-04-20 21:10:35 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "std_lib_facilities.h"
+
+//----------------------------------------------------------------------
+
+int main()
+try
+{
+}
+catch (Range_error &re)
+{
+    cerr << "Bad index: " << re.index << "\n";
+}
+catch (const std::exception &e)
+{
+    cerr << "Exception: " << e.what() << '\n';
+    return 1;
+}
+catch (...)
+{
+    cerr << "Some kind of exception\n";
+    return 2;
+}
