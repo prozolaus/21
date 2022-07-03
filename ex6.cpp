4bfcfa6b92debab4d22a97b392eee708a453fd1c
Nikolai Prozorov <prozorovmykola@gmail.com>
2022-07-03 12:45:28 +0300

    ex6: set<Fruit *, Fruit_comp>

---
+++ ex6.cpp
@@ -0,0 +1,54 @@
+#include "std_lib_facilities.h"
+#include <set>
+
+struct Fruit
+{
+    string name;
+    int count;
+    double unit_price;
+};
+//----------------------------------------------------------------------
+
+struct Fruit_order_by_name
+{
+    bool operator()(const Fruit *fruit1, const Fruit *fruit2) const
+    {
+        return fruit1->name < fruit2->name;
+    }
+};
+//----------------------------------------------------------------------
+
+ostream &operator<<(ostream &os, const Fruit *fruit)
+{
+    cout << "Fruit: " << fruit->name << ", count: " << fruit->count << ", price: " << fruit->unit_price;
+    return os;
+}
+
+//----------------------------------------------------------------------
+
+int main()
+try
+{
+    set<Fruit *, Fruit_order_by_name> inventory;
+    inventory.insert(new Fruit{"quince", 5});
+    inventory.insert(new Fruit{"apple", 200, 0.37});
+    for (auto p = inventory.begin(); p != inventory.end(); ++p)
+        cout << *p << endl;
+    // cleaning up
+    for (auto p = inventory.begin(); p != inventory.end(); ++p)
+        delete *p;
+}
+catch (Range_error &re)
+{
+    cerr << "Bad index: " << re.index << "\n";
+}
+catch (const std::exception &e)
+{
+    cerr << "Exception: " << e.what() << '\n';
+    return 1;
+}
+catch (...)
+{
+    cerr << "Some kind of exception\n";
+    return 2;
+}
